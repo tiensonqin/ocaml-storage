ac03bd607566230ea5c08b5098275cf95e403d7f
Tienson Qin <tiensonqin@gmail.com>
2020-02-05 19:45:38 +0800

    hamt wip

---
+++ hamt/lib/hamt.ml
@@ -0,0 +1,122 @@
+(* TODO: relax this using module *)
+type key = string
+
+type 'a t = {
+  mutable length: int;
+  internal: 'a internal
+}
+and 'a internal =
+  | Empty
+  | Leaf of key * 'a
+  | Node of 'a internal array
+
+let array_len = 32
+
+(* TODO: remove int restriction *)
+let empty : int t = {
+  length = 0;
+  internal = Empty
+}
+
+let leaf k v = Leaf (k, v)
+
+let node children = Node children
+
+let extract_children = function
+  | Node children -> children
+  | _ -> [||]
+
+let hash = Base.String.hash
+
+let segments n base =
+  let safe_wrap x =
+    if x = -1 then base - 1 else x in
+  let rec loop n acc =
+    if n < base then
+      (safe_wrap (n - 1)) :: acc
+    else
+      let acc = (safe_wrap (n mod base - 1)) :: acc in
+      loop (n / base) acc in
+  loop n []
+
+exception Something_wrong
+
+let is_node = function
+  | Node (_) -> true
+  | _ -> false
+
+let insert children segments k v =
+  let rec loop children = function
+    | idx :: [] ->
+      begin
+        match Array.get children idx with
+        | Empty ->
+          Array.set children idx (leaf k v)
+        | _ ->
+          raise Something_wrong
+      end
+    | idx :: tl ->
+      if not (is_node children.(idx)) then
+        Array.set children idx (node (Array.make array_len Empty));
+      loop (extract_children children.(idx)) tl
+    | [] ->
+      ()
+  in
+  loop children segments
+
+(* path copy *)
+let assoc k v {length; internal} =
+  match internal with
+  | Empty ->
+    {
+      length = 1;
+      internal = leaf k v
+    }
+  | Leaf (k2, v2) ->
+    if k2 = k then
+      {
+        length = 1;
+        internal = leaf k v
+      }
+    else
+      let children = Array.make array_len Empty in
+      let segments1 = segments (hash k) array_len in
+      let segments2 = segments (hash k2) array_len in
+      let _ = insert children segments1 k v in
+      let _ = insert children segments2 k2 v2 in
+      {
+        length = 2;
+        internal = Node children
+      }
+  | Node children ->
+    {length;
+     internal = Node children
+    }
+
+let find k {internal; _} =
+  let segments = segments (hash k) array_len in
+  let rec loop children l =
+    match l with
+    | [] -> None
+    | (current :: next) ->
+      match children with
+      | Empty -> None
+      | Leaf (k', v) ->
+        if k = k' then Some v else None
+      | Node children ->
+        loop children.(current) next in
+  loop internal segments
+
+(* let update _k _v _t = ()
+ * let dissoc _k _t = () *)
+
+let length {length; _} =
+  length
+
+(*
+open Hamt;;
+let m = empty;;
+let m1 = assoc "a" 1 m;;
+let m2 = assoc "b" 2 m1;;
+find "b" m2;;
+*)
